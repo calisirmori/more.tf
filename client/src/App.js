00dbfa54c65f6214ebdff6623ed1fac036f04d90
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2022-11-16 13:30:05 -0700

    unzip in memory to read the text without saving

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch("https://logs.tf/logs/log_3303850.log.zip").then((r)=>{r.text().then((input)=>{
+
+    console.log(typeof d)
+
+  })})
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        hi
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,45 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+var http = require("http"),
+    zlib = require("zlib");
+var yauzl = require("yauzl");
+var request = require('request');
 
 app.use(express.json());
 app.use(cors());
+
+var file_url = 'http://logs.tf/logs/log_3303850.log.zip';
+
+var AdmZip = require('adm-zip');
+var request = require('request');
+
+
+http.get(file_url, function(res) {
+  var data = [], dataLen = 0; 
+
+  res.on('data', function(chunk) {
+    data.push(chunk);
+    dataLen += chunk.length;
+
+  }).on('end', function() {
+    var buf = Buffer.alloc(dataLen);
+
+    for (var i = 0, len = data.length, pos = 0; i < len; i++) { 
+      data[i].copy(buf, pos); 
+      pos += data[i].length; 
+    } 
+
+    var zip = new AdmZip(buf);
+    var zipEntries = zip.getEntries();
+    console.log(zipEntries.length)
+
+    for (var i = 0; i < zipEntries.length; i++) {
+      console.log(zipEntries[i].getData().toString())
+      if (zipEntries[i].entryName.match(/readme/))
+        console.log(zip.readAsText(zipEntries[i]));
+    }
+  });
+});
 
 app.listen(8080, () => console.log('Server is running on : 8080'));
