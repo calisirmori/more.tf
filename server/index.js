4258f373b5a0010d5db3506fc01be7155e92fab8
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2022-11-21 02:19:24 -0700

    backend api call ready

--- server/index.js
+++ server/index.js
@@ -4,21 +4,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require("http");
 const AdmZip = require('adm-zip');
-const inputToJson = require('./inputToJson');
-const PlayerDamageParser = require('./src/PlayerDamageParser');
 const logstfApiOrganizer = require('./src/logstfApiOrganizer')
 const axios = require('axios');
 
 app.use(express.json());
 app.use(cors());
 
-let matchId = 3304362;
+app.get('/logsplus/:id', async (req, result, next) => {
 
-async function logstfCalls(){
+  const matchId = req.params.id;
   let logsApiResponse = await axios.get(`https://logs.tf/api/v1/log/${matchId}`);
 
-  http.get(`http://logs.tf/logs/log_${matchId}.log.zip`, async function(res) {
-    var data = [], dataLen = 0; 
+  http.get(`http://logs.tf/logs/log_${matchId}.log.zip`, function(res) {
+    let data = [], dataLen = 0; 
     
     res.on('data', function(chunk) {
 
@@ -27,57 +25,22 @@ async function logstfCalls(){
   
     }).on('end', async function() {
 
-      var buf = Buffer.alloc(dataLen);
-      
-      for (var i = 0, len = data.length, pos = 0; i < len; i++) { 
+      let buf = Buffer.alloc(dataLen);
+  
+      for (let i = 0, len = data.length, pos = 0; i < len; i++) { 
         data[i].copy(buf, pos); 
         pos += data[i].length; 
       } 
 
-      var zip = new AdmZip(buf);
-      var zipEntries = zip.getEntries();
+      let zip = new AdmZip(buf);
+      let zipEntries = zip.getEntries();
+      let textFile = zipEntries[0].getData().toString();
 
-      var textFile = zipEntries[0].getData().toString();
-      //console.log(inputToJson.stringToObject(textFile));
-      //console.log(PlayerDamageParser.damageParser(textFile))
-       console.log(await logstfApiOrganizer.organize(logsApiResponse,textFile,matchId))
-    });
-  });
-}
+      result.json(await logstfApiOrganizer.organize(logsApiResponse,textFile,matchId));
 
-logstfCalls();
-
-
-app.get('/logsplus/:id', (req, result, next) => {
-  const matchId = req.params.id;
-  matchId = 3306523;
-  const url = `http://logs.tf/logs/log_${matchId}.log.zip`
-
-  http.get(url, function(res) {
-    var data = [], dataLen = 0; 
-    
-    res.on('data', function(chunk) {
-
-      data.push(chunk);
-      dataLen += chunk.length;
-  
-    }).on('end', function() {
-
-      var buf = Buffer.alloc(dataLen);
-  
-      for (var i = 0, len = data.length, pos = 0; i < len; i++) { 
-        data[i].copy(buf, pos); 
-        pos += data[i].length; 
-      } 
-
-      var zip = new AdmZip(buf);
-      var zipEntries = zip.getEntries();
-
-      var textFile = zipEntries[0].getData().toString();
-      console.log(inputToJson.stringToObject(textFile));
-      result.json(inputToJson.stringToObject(textFile));
     });
   });
 });
 
 app.listen(8080, () => console.log('Server is running on : 8080'));
+
