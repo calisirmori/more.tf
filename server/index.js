cef53d33fd16569667cda15bd6ec37166ec964d6
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2022-12-02 17:19:03 -0700

    port

--- server/index.js
+++ server/index.js
@@ -57,5 +57,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8000, () => console.log('Server is running on : 8080'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
