91f32091756b3f3cdfadbd79e7e2609b30d69d1f
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2022-12-04 23:37:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/logsplus/:id', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
