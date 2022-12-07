13abe3cbb71f91863d2cd564b68cffee9f055298
c43721 <rlahman.game@gmail.com>
2022-12-06 18:40:30 -0600

    chore(*): get off of cra

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ app.get("/logsplus/:id", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, function () {
