9ae04a6c29f63c7703886a941ee6337381df9dfb
c43721 <rlahman.game@gmail.com>
2022-12-06 18:23:51 -0600

    feat(*): cleanup backend slightly

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const AdmZip = require('adm-zip');
-const logstfApiOrganizer = require('./src/logstfApiOrganizer')
-const { summaryObject } = require('./summary');
-const seasonSummary = require('./src/seasonSummary.js');
-const path = require('path');
-const { fetch, FetchResultTypes } = require('@sapphire/fetch');
-
+const cors = require("cors");
+const AdmZip = require("adm-zip");
+const logstfApiOrganizer = require("./src/logstfApiOrganizer");
+const { summaryObject } = require("./summary");
+const seasonSummary = require("./src/seasonSummary.js");
+const path = require("path");
+const { fetch, FetchResultTypes } = require("@sapphire/fetch");
 
 app.use(express.json());
 app.use(cors());
@@ -16,38 +15,53 @@ app.use(cors());
 //this is used to create seasonal summary text files
 //console.log( seasonSummary.makeSummary())
 
-app.get('/api/season-13-summary', (req, result) => {
+app.get("/api/season-13-summary", (_, result) => {
   result.json(summaryObject);
 });
 
-app.get('/logsplus/:id', async (req, res, error) => {
+app.get("/logsplus/:id", async (req, res) => {
   let matchId = req.params.id;
-  matchId = parseInt(matchId)
-  if( !isNaN(parseInt(matchId, 10)) > 0 && matchId < Number.MAX_SAFE_INTEGER){
-    try {
-      const logsApiResponse = await fetch(`https://logs.tf/api/v1/log/${matchId}`, FetchResultTypes.JSON);
+  matchId = parseInt(matchId);
 
-      const buffer = await fetch(`http://logs.tf/logs/log_${matchId}.log.zip`, FetchResultTypes.Buffer);
-      
-      const zip = new AdmZip(buffer);
-      const zipEntries = zip.getEntries();
-      const textFile = zipEntries[0].getData().toString();
-  
-      res.json(await logstfApiOrganizer.organize(logsApiResponse, textFile, matchId));
-    } catch (error) {
-      res.status(404).json('Bad Log ID');
-    }
-    
+  if (!isNaN(parseInt(matchId, 10)) > 0 && matchId < Number.MAX_SAFE_INTEGER) {
+    res
+      .status(400)
+      .json({ errorCode: 400, message: "Bad logs ID", error: "Bad Request" });
+  }
+
+  try {
+    const logsApiResponse = await fetch(
+      `https://logs.tf/api/v1/log/${matchId}`,
+      FetchResultTypes.JSON
+    );
+
+    const buffer = await fetch(
+      `http://logs.tf/logs/log_${matchId}.log.zip`,
+      FetchResultTypes.Buffer
+    );
+
+    const zip = new AdmZip(buffer);
+    const zipEntries = zip.getEntries();
+    const textFile = zipEntries[0].getData().toString();
+
+    res.json(
+      await logstfApiOrganizer.organize(logsApiResponse, textFile, matchId)
+    );
+  } catch (error) {
+    res.status(500).json({ errorCode: 500, message: "Internal Server Error" });
   }
 });
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
-
