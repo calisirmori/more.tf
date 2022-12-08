883113372d2b3ff40ef34e1f32abc799c72c6a76
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2022-12-08 00:33:00 -0700

    player page initialize

--- client/src/components/PlayerProfile/PlayerProfile.jsx
+++ client/src/components/PlayerProfile/PlayerProfile.jsx
@@ -1,30 +1,249 @@
-import React from 'react'
-import { CheckBox, LogsHeader, LogsList, MatchDate, MatchFormat, MatchId, MatchLogCard, MatchTitle, MathMap, MostRecentMatch, PlayerInfo, PlayerMatchLogs, PlayerProfileWrapper, Profile, ProfileSections } from './PlayerProfileStyles';
+import { fetch, FetchResultTypes } from '@sapphire/fetch';
+import React, { useEffect, useState } from 'react'
+import { CheckBox, ClassPlayed, ColorBox, Damage, DateAndID, Format, FormatFooter, FormatHeader, FormatPercentage, FormatText, FormatWrapper, GameDate, KDA, LogInfo, LogsHeader, LogsList, MapPlayed, MatchDate, MatchFormat, MatchID, MatchId, MatchInfo, MatchLogCard, MatchTitle, MathMap, MostRecentMatch, PercentageBar, PlayerFunFact, PlayerInfo, PlayerLink, PlayerLinks, PlayerMatchLogs, PlayerProfileWrapper, PlayerStats, Profile, ProfilePicture, ProfileSections, Score, ScoreInfo, SectionHeader, StatHeader, StatInfo, StatWrapper, SteamInfo, Username } from './PlayerProfileStyles';
 
 const PlayerProfile = () => {
-  return (
-    <PlayerProfileWrapper>
-        <ProfileSections>
-            <PlayerInfo>
-                <Profile></Profile>
-                <MostRecentMatch></MostRecentMatch>
-            </PlayerInfo>
-            <PlayerMatchLogs>
-                <LogsHeader></LogsHeader>
-                <LogsList>
-                    <MatchLogCard>
-                        <CheckBox></CheckBox>
-                        <MatchId></MatchId>
-                        <MatchTitle></MatchTitle>
-                        <MathMap></MathMap>
-                        <MatchFormat></MatchFormat>
-                        <MatchDate></MatchDate>
-                    </MatchLogCard>
-                </LogsList>
-            </PlayerMatchLogs>
-        </ProfileSections>
-    </PlayerProfileWrapper>
-  )
+    const [apiResponse, setApiResponse] = useState({});
+    const [currentPage, setCurrentPage] = useState(0);
+    const [currentLogs, setCurrentLogs] = useState([]);
+    const [lastMatchWon, setLastMatchWon] = useState(true);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const [lastLogResponse, setLastLogResponse] = useState({});
+    const [formatObject, setFormatObject] = useState({});
+    const playerID3 = "[U:1:" + (1198068401396-1197960265728) + "]";
+
+    
+
+    useEffect(() => {
+        try {
+            const lastLogId = apiResponse.logs[0].id
+            let logsArray= apiResponse.logs.splice(currentPage, 25);
+            setCurrentLogs(logsArray)
+            logstfApiCall(lastLogId);
+        } catch (error) {
+            console.log("not yet")
+        }
+        
+    }, [apiResponse])
+
+    useEffect(() => {
+        apiCall();
+    }, [])
+
+    let format = {
+        total: 0,
+        hl : 0,
+        sixes : 0,
+        fours: 0,
+        UandBBAL : 0,
+    }
+    async function apiCall(map, players){
+        let response = await fetch(`https://logs.tf/api/v1/log?player=76561198068401396&limit=10000`, FetchResultTypes.JSON);
+        
+        format = {
+            total: 0,
+            hl : 0,
+            sixes : 0,
+            fours: 0,
+            UandBBAL : 0,
+        }
+
+        response.logs.map((log)=>{
+            if (log.players > 3 && log.players < 6){
+                format.UandBBAL++;
+                format.total++;
+            } else if (log.players >= 6 && log.players < 11){
+                format.fours++;
+                format.total++;
+            } else if (log.players >= 11 && log.players <15){
+                format.sixes++;
+                format.total++;
+            } else if (log.players >= 15 && log.players <22){
+                format.hl++;
+                format.total++;
+            } 
+        })
+
+        setFormatObject(format);
+        setApiResponse(response);
+    }
+    
+    async function logstfApiCall(matchId){
+        const logsApiResponse = await fetch(
+            `https://logs.tf/api/v1/log/${matchId}`,
+            FetchResultTypes.JSON
+        );
+
+        try {
+            if(lastLogResponse.teams.Blue.score > lastLogResponse.teams.Red.score && lastLogResponse.players[playerID3].team === "Blue"){
+                setLastMatchWon(true);
+            } else if (lastLogResponse.teams.Red.score > lastLogResponse.teams.Blue.score && lastLogResponse.players[playerID3].team === "Red"){
+                setLastMatchWon(true);
+            } else {
+                setLastMatchWon(false);
+            }
+        } catch (error) {
+            
+        }
+
+        setLastLogResponse(logsApiResponse)
+    }
+    function formatFinder(players){
+        if (players > 3 && players < 6){
+            return ("U/B");
+        } else if (players >= 6 && players < 11){
+            return ("4s");
+        } else if (players >= 11 && players <15){
+            return ("6s");
+        } else if (players >= 15 && players <22){
+            return ("HL");
+        } else {
+            return ("?");
+        }
+    }
+
+    function DamageIconMaker(input,team) {
+        switch (input) {
+          case "scout":
+           return "https://wiki.teamfortress.com/w/images/a/ad/Leaderboard_class_scout.png";
+      
+          case "soldier":
+           return "https://wiki.teamfortress.com/w/images/9/96/Leaderboard_class_soldier.png";
+      
+          case "pyro":
+           return "https://wiki.teamfortress.com/w/images/8/80/Leaderboard_class_pyro.png";
+      
+          case "demoman":
+           return "https://wiki.teamfortress.com/w/images/4/47/Leaderboard_class_demoman.png";
+      
+          case "heavyweapons":
+           return "https://wiki.teamfortress.com/w/images/5/5a/Leaderboard_class_heavy.png";
+      
+          case "engineer":
+           return "https://wiki.teamfortress.com/w/images/1/12/Leaderboard_class_engineer.png";
+      
+          case "medic":
+           return "https://wiki.teamfortress.com/w/images/e/e5/Leaderboard_class_medic.png";
+      
+          case "sniper":
+            return "https://wiki.teamfortress.com/w/images/f/fe/Leaderboard_class_sniper.png";
+      
+          case "spy":
+            return "https://wiki.teamfortress.com/w/images/3/33/Leaderboard_class_spy.png";
+        };
+    };
+    let current = 0;
+    if(lastLogResponse.players !== undefined){
+        return (
+            <PlayerProfileWrapper>
+                <ProfileSections>
+                    <PlayerInfo>
+                        <Profile>
+                            <SteamInfo>
+                                <ProfilePicture src="https://avatars.akamai.steamstatic.com/0462901bf034ef06615019cdb2bbfc9bc747b256_full.jpg"></ProfilePicture>
+                                <Username>mori</Username>
+                            </SteamInfo>
+                            <PlayerLinks>
+                                <PlayerLink href="google.com" target="_blank">STEAM</PlayerLink>
+                                <PlayerLink href="google.com" target="_blank">RGL</PlayerLink>
+                                <PlayerLink href="google.com" target="_blank">UGC</PlayerLink>
+                                <PlayerLink href="google.com" target="_blank">ETF2L</PlayerLink>
+                                <PlayerLink href="google.com" target="_blank">OZ</PlayerLink>
+                            </PlayerLinks>
+                            <PlayerFunFact>
+                                <LogInfo>
+                                    <StatWrapper>
+                                        <StatHeader>Last Match</StatHeader>
+                                        <StatInfo>01/04/2120</StatInfo>
+                                    </StatWrapper>
+                                    <StatWrapper>
+                                        <StatHeader>First Match</StatHeader>
+                                        <StatInfo>01/04/2120</StatInfo>
+                                    </StatWrapper>
+                                    <StatWrapper>
+                                        <StatHeader>Total Matches</StatHeader>
+                                        <StatInfo>3215</StatInfo>
+                                    </StatWrapper>
+                                </LogInfo>
+                                <FormatPercentage>
+                                    <FormatHeader>FORMATS PLAYED</FormatHeader>
+                                    <PercentageBar style={{gridTemplateColumns: `${(formatObject.sixes/formatObject.total)*450}px ${(formatObject.hl/formatObject.total)*450}px ${(formatObject.fours/formatObject.total)*450}px ${(formatObject.UandBBAL/formatObject.total)*450}px`}}>
+                                        <Format style={{background: "#8650AC"}}>{Math.round(formatObject.sixes/formatObject.total*100)+"%"}</Format>
+                                        <Format style={{background: "#4D7455"}}>{Math.round(formatObject.hl/formatObject.total*100) > 10 ? Math.round(formatObject.hl/formatObject.total*100)+"%" : ""}</Format>
+                                        <Format style={{background: "#476291"}}>{Math.round(formatObject.fours/formatObject.total*100) > 10 ? Math.round(formatObject.fours/formatObject.total*100)+"%" : ""}</Format>
+                                        <Format style={{background: "#BD3B3B"}}>{Math.round(formatObject.UandBBAL/formatObject.total*100) > 10 ? Math.round(formatObject.UandBBAL/formatObject.total*100)+"%" : ""}</Format>
+                                    </PercentageBar>
+                                    <FormatFooter>
+                                        <FormatWrapper>
+                                            <ColorBox style={{background: "#8650AC"}}></ColorBox>
+                                            <FormatText> 6s </FormatText>
+                                        </FormatWrapper>
+                                        <FormatWrapper>
+                                            <ColorBox style={{background: "#4D7455"}}></ColorBox>
+                                            <FormatText> HL</FormatText>
+                                        </FormatWrapper>
+                                        <FormatWrapper>
+                                            <ColorBox  style={{background: "#476291"}}></ColorBox>
+                                            <FormatText> 4s </FormatText>
+                                        </FormatWrapper>
+                                        <FormatWrapper>
+                                            <ColorBox  style={{background: "#BD3B3B"}}></ColorBox>
+                                            <FormatText>Ultiduo/BBAL</FormatText>
+                                        </FormatWrapper>
+                                    </FormatFooter>
+                                    <FormatHeader>MEDALS</FormatHeader>
+                                    
+                                </FormatPercentage>
+                            </PlayerFunFact>
+                        </Profile>
+                        <MostRecentMatch style={lastMatchWon === true ? {borderBottom: "5px solid green"}:{borderBottom: "5px solid red"}}>
+                            <SectionHeader>LAST MATCH PLAYED</SectionHeader>
+                            <MatchInfo>
+                                <ClassPlayed src={DamageIconMaker(lastLogResponse.players[playerID3].class_stats[0].type)}></ClassPlayed>
+                                <ScoreInfo>
+                                    <Score>{`${lastLogResponse.teams.Blue.score} - ${lastLogResponse.teams.Red.score}`}</Score>
+                                    <MapPlayed>{lastLogResponse.info.map}</MapPlayed>
+                                </ScoreInfo>
+                                <DateAndID>
+                                    <MatchID>{"#"+apiResponse.logs[0].id}</MatchID>
+                                    <GameDate>{(new Date(lastLogResponse.info.date * 1000)).toLocaleDateString('en-US')}</GameDate>
+                                </DateAndID>
+                            </MatchInfo>
+                            <PlayerStats>
+                                <KDA tooltip="Kills/Deaths/Assists">{`${lastLogResponse.players[playerID3].kills} / ${lastLogResponse.players[playerID3].deaths} / ${lastLogResponse.players[playerID3].assists} `}</KDA>
+                                <Damage tooltip="damage | dpm">{`${lastLogResponse.players[playerID3].dmg} | ${lastLogResponse.players[playerID3].dapm} `}</Damage>
+                            </PlayerStats>
+                        </MostRecentMatch>
+                    </PlayerInfo>
+                    <PlayerMatchLogs>
+                        <LogsHeader></LogsHeader>
+                        <LogsList>
+                            {currentLogs.map((log)=>{
+                                return(
+                                    <MatchLogCard to={`/log/${log.id}`} style={current++ % 2 === 1 ? {background: "#1f1f1f"}: {}}>
+                                        <CheckBox>x</CheckBox>
+                                        <MatchId>{log.id}</MatchId>
+                                        <MatchTitle>{log.title}</MatchTitle>
+                                        <MathMap>{log.map}</MathMap>
+                                        <MatchFormat>{formatFinder(log.players)}</MatchFormat>
+                                        <MatchDate>{`${(new Date(log.date * 1000)).toLocaleString("en-US", options).split(",").splice(1,2) + " " + (new Date(log.date * 1000)).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`}</MatchDate>
+                                    </MatchLogCard>
+                                )
+                            })}
+                        </LogsList>
+                    </PlayerMatchLogs>
+                </ProfileSections>
+            </PlayerProfileWrapper>
+        )
+    } else {
+        return (
+            <div id="loader">
+              <div id="shadow"></div>
+              <div id="box"></div>
+            </div>
+          )
+    }
+  
 }
 
 export default PlayerProfile;
--- client/src/components/PlayerProfile/PlayerProfileStyles.jsx
+++ client/src/components/PlayerProfile/PlayerProfileStyles.jsx
@@ -1,18 +1,304 @@
 import styled from 'styled-components'
+import {Link} from 'react-router-dom';
 
-export const PlayerProfileWrapper = styled.div``;
-export const ProfileSections = styled.div``;
-export const PlayerInfo = styled.div``;
-export const Profile = styled.div``;
-export const MostRecentMatch = styled.div``;
-export const PlayerMatchLogs = styled.div``;
-export const LogsHeader = styled.div``;
-export const LogsList = styled.div``;
-export const MatchLogCard = styled.div``;
-export const CheckBox = styled.div``;
-export const MatchId = styled.div``;
-export const MatchTitle = styled.div``;
-export const MathMap = styled.div``;
-export const MatchFormat = styled.div``;
-export const MatchDate = styled.div``;
-export const Example = styled.div``;
+export const PlayerProfileWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Poppins";
+`;
+
+export const ProfileSections = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const PlayerInfo = styled.div`
+    display: block;
+`;
+
+export const Profile = styled.div`
+    background: #171616;
+    color: #fff;
+    border-radius: 5px;
+    height: 530px;
+    width: 480px;
+    padding: 10px;
+    display: block;
+    
+`;
+
+export const MostRecentMatch = styled.div`
+
+    margin-top: 20px;
+    background: #171616;
+    color: #fff;
+    border-radius: 5px;
+    height: 225px;
+    width: 480px;
+    padding: 10px;
+    display: block;
+`;
+
+export const PlayerMatchLogs = styled.div`
+    color: #fff;
+    font-weight: 200;
+    border-radius: 5px;
+    height: 820px;
+    width: 1000px;
+    margin-left: 20px;
+`;
+
+export const LogsHeader = styled.div`
+    background: #000;
+    border-radius: 5px;
+    height: 110px;
+    width: 980px;
+    margin-bottom: 10px;
+`;
+
+export const LogsList = styled.div`
+    padding: 10px;
+    background: #171616;
+    border-radius: 5px;
+    height: 680px;
+    width: 960px;
+    display: grid;
+    grid-template-rows: repeat(20, 1fr);
+`;
+
+export const MatchLogCard = styled(Link)`
+    text-decoration: none;
+    color: #fff;
+    background: #171616;
+    display: grid;
+    text-align: center;
+    grid-template-columns: 20px 80px 1fr 180px 50px 250px;
+    cursor: pointer;
+    border: 1px solid #171616;
+    
+    :hover{
+        border: 1px solid #f08149;
+    }
+`;
+
+export const CheckBox = styled.div`
+    
+`;
+
+export const MatchId = styled.div`
+    border-left: 2px solid #f08149;
+`;
+
+export const MatchTitle = styled.div`
+    text-align: left;
+    padding-left: 10px;
+    border-left: 2px solid #f08149;
+`;
+
+export const MathMap = styled.div`
+    border-left: 2px solid #f08149;
+`;
+
+export const MatchFormat = styled.div`
+    border-left: 2px solid #f08149;
+`;
+
+export const MatchDate = styled.div`
+    border-left: 2px solid #f08149;
+`;
+
+export const MatchInfo = styled.div`
+    border-top: 1px solid #fff;
+    margin: 5px 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    width: 480px;
+`;
+
+export const ClassPlayed = styled.img`
+    margin: 10px;
+    
+    height: 60px;
+`;
+
+export const ScoreInfo = styled.div`
+    text-align: center;
+`;
+
+export const MapPlayed = styled.div`
+    margin-top: -10px;
+    font-size: 14px;
+    font-weight: 300;
+`;
+
+export const MatchID = styled.div`
+    color: #f08149;
+`;
+
+export const Score = styled.div`
+    font-size: 30px;
+    font- weight: 700;
+`;
+
+export const GameDate = styled.div`
+
+`;
+
+export const DateAndID = styled.div`
+    text-align: right;
+    margin-right: 10px;
+`;
+
+export const PlayerStats = styled.div`
+
+`;
+
+export const KDA = styled.div`
+    border-top: 1px solid #fff;
+    padding-top: 5px;
+    font-size: 50px;
+    text-align: center;
+    font-weight: 800;
+`;
+
+export const Damage = styled.div`
+    font-size: 30px;
+    text-align: center;
+    font-weight: 800;
+    margin-top: -10px;
+`;
+
+
+
+export const SectionHeader = styled.div`
+    font-weight: 800;
+    text-align: center;
+`;
+export const SteamInfo = styled.div`
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    margin: 20px 45px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-top: 10px;
+    align-items: center;
+`;
+
+export const ProfilePicture = styled.img`
+    height: 64px;
+    border-radius: 0;
+`;
+
+export const Username = styled.div`
+    text-align: center;
+    font-weight: 800;
+    font-size: 40px;
+`;
+
+export const PlayerLinks = styled.div`
+    margin-left: 45px;
+    display: grid;
+    grid-template-columns: repeat(5, 80px);
+`;
+
+export const PlayerLink = styled.a`
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 3px;
+    background-color: #34302d;
+    height: 30px;
+    margin-right: 5px;
+    cursor: pointer;
+    border-radius: 3px;
+    text-decoration: none;
+    color: #fff;
+    :hover {
+        outline: 1px solid #f08149;
+        background-color: #1d1c1c;
+    }
+    
+`;
+
+export const PlayerFunFact = styled.div`
+    display: block;
+`;
+
+export const LogInfo = styled.div`
+    display: grid;
+    margin: 30px 45px;
+    grid-template-columns: repeat(3, 1fr);
+    text-align: center;
+`;
+
+export const StatWrapper = styled.div`
+    
+`;
+
+export const StatHeader = styled.div`
+    font-weight: bold;
+`;
+
+export const StatInfo = styled.div`
+    font-weight: 200;
+`;
+
+export const FormatPercentage = styled.div`
+
+`;
+
+export const Format = styled.div`
+    padding-left: 10px;
+`;
+
+export const FormatHeader = styled.div`
+    text-align: center;
+    margin-bottom: 10px;
+    font-weight: bold;
+    font-size: 20px;
+    padding-top: 10px;
+    border-top: 2px solid #fff;
+`;
+
+export const PercentageBar = styled.div`
+    display: grid;
+    margin-left: 20px;
+    
+`;
+
+export const FormatFooter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    padding-bottom: 15px;
+`;
+
+export const FormatWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10px;
+`;
+
+export const ColorBox = styled.div`
+    width: 15px;
+    height: 15px;
+    
+    margin-right: 5px;
+    margin-left: 20px;
+`;
+
+export const FormatText = styled.div`
+
+`;
+
+export const Example = styled.div`
+
+`;
+
