5f6e18ddc1281e1be034f34699bceb883613fa0d
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2023-01-23 15:32:56 -0700

    navbar logo import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import moretfWhiteLogo from "./assets/moretf-white-medium.png";
 
 function App() {
 
 
   return (
     <div className='bg-stone-900 min-h-screen'>
+      <img className="" src={moretfWhiteLogo} alt="logo" />
     </div>
   )
 }
