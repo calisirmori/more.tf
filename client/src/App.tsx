011489d2cc17e7c184f133cebbbf830cab4b9d42
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2023-01-23 14:49:02 -0700

    client folder initiate

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className=' text-blue-300'> hello
+    </div>
+  )
+}
+
+export default App
