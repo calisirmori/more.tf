282c29501e9bb88769a20bb6286e5edde0207ed3
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2023-01-23 20:11:55 -0700

    hero layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,9 +10,20 @@ function App() {
       <div className=" absolute z-10 w-full top-0 pt-3">
         <Navbar />
       </div>
-      <div className="bg-black w-full h-[36rem]  absolute z-0 top-0">
-        <img src="https://i.imgur.com/0YGCnmh.jpeg" alt="" className=" absolute h-[36rem] object-cover w-full z-0 opacity-10 top-0" />
-        <div>hey</div>
+      <div className="bg-black w-full h-[40rem] absolute z-0 top-0">
+        <img src="https://i.imgur.com/0YGCnmh.jpeg" alt="" className=" absolute h-[40rem] object-cover w-full z-0 opacity-10 top-0" />
+        <div className="text-white absolute z-10 top-16">
+          <div className="grid grid-cols-2 w-screen h-96 gap-10 px-72 my-20">
+            <div className="border border-pink-500">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="border border-pink-500"></div>
+            
+          </div>
+        </div>
       </div>
       
     </div>
