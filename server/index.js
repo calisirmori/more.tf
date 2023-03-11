9b8bf4a13fc376e4f8a183d3dfe0e23884a12189
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2023-03-11 15:11:46 -0700

    deploy no directory

--- server/index.js
+++ server/index.js
@@ -271,10 +271,10 @@ app.get("/api/log/:id", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
 });
 
 app.listen(port, function () {
