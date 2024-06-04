7eb75cb888fdffeed00234fa4ee66be7a1951544
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2024-06-03 21:08:49 -0700

    navbar for this season

--- client/src/components/shared-components/Navbar.tsx
+++ client/src/components/shared-components/Navbar.tsx
@@ -101,10 +101,10 @@ const Navbar = () => {
                 </div>
                 <div className="">
                   <div className=" text-lightscale-5 text-lg font-semibold  hover:text-lightscale-1 mt-10 duration-200 flex justify-center">
-                    <a href="/season-summary/147" className="">HL SUMMARY</a>
+                    <a href="/season-summary/151" className="">HL SUMMARY</a>
                   </div>
                   <div className=" text-lightscale-5 text-lg font-semibold  hover:text-lightscale-1 mt-10 duration-200 flex justify-center">
-                    <a href="/season-summary/148" className="">6S SUMMARY</a>
+                    <a href="/season-summary/153" className="">6S SUMMARY</a>
                   </div>
                   <div className=" text-lightscale-5  text-lg  font-semibold  hover:text-lightscale-1 mt-6 duration-200 flex justify-center">
                     <a href="https://discord.gg/Zb5BEUy9Fb" target="_blank" className="">CONTACT</a>
@@ -128,10 +128,10 @@ const Navbar = () => {
             />
           </a>
           <div className=" text-lightscale-5 text-sm font-semibold md:ml-20 max-md:ml-10 max-sm:ml-0 max-sm:scale-0 max-sm:w-0 hover:text-lightscale-1 duration-200">
-            <a href="/season-summary/147" className="">HL SUMMARY</a>
+            <a href="/season-summary/151" className="">HL SUMMARY</a>
           </div>
           <div className=" text-lightscale-5  text-sm  font-semibold md:ml-10 max-md:ml-5 max-sm:ml-0  max-sm:scale-0 max-sm:w-0 hover:text-lightscale-1 duration-200">
-            <a href="/season-summary/148" className="">6S SUMMARY</a>
+            <a href="/season-summary/153" className="">6S SUMMARY</a>
           </div>
           <div className=" text-lightscale-5  text-sm  font-semibold md:ml-10 max-md:ml-5 max-sm:ml-0  max-sm:scale-0 max-sm:w-0 hover:text-lightscale-1 duration-200">
             <a href="https://discord.gg/Zb5BEUy9Fb" target="_blank" className="">CONTACT</a>
