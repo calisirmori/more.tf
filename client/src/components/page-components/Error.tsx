bfdc905a38510fe2a64b21c14945b8d5f560bf75
Bobby Joe <jobbyboe987@gmail.com>
2025-01-20 21:48:02 -0500

    Made link to GitHub go to issues tab

--- client/src/components/page-components/Error.tsx
+++ client/src/components/page-components/Error.tsx
@@ -7,7 +7,7 @@ const Error = () => {
             <Navbar />
             <div className="h-full w-full flex-col justify-center space-y-6 items-center px-20 flex">
                 <h1 className="text-5xl">We've run into a problem. :&#40;</h1>
-                <p className="">We hit some kind of error. Let us know at <a className="underline hover:text-tf-orange-dark transition duration-200" target="_blank" href="https://github.com/calisirmori/more.tf">our GitHub</a> and we'll look into it further.</p>
+                <p className="">We hit some kind of error. Let us know at <a className="underline hover:text-tf-orange-dark transition duration-200" target="_blank" href="https://github.com/calisirmori/more.tf/issues">our GitHub</a> and we'll look into it further.</p>
             </div>
             <Footer />
         </div>
